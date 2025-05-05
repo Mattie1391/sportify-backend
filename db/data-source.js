2df613c503a8060c4d74b15d821e1039fb1b0afc
Arsian Kuo <arsiankuo86@gmail.com>
2025-05-06 03:45:02 +0800

    fix: 部署render，調整環境設定

--- db/data-source.js
+++ db/data-source.js
@@ -1,14 +1,15 @@
 const { DataSource } = require("typeorm");
-require("dotenv").config();
+const db_config = require("../config/db");
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  synchronize: false, //開發時 true，部署時請改為 false 並使用 migration
+  host: db_config.host,
+  port: db_config.port,
+  username: db_config.username,
+  password: db_config.password,
+  database: db_config.database,
+  ssl: db_config.ssl,
+  synchronize: db_config.synchronize, // 開發時 true，部署時請改為 false 並使用 migration
   logging: true,
   entities: [
     require("../entities/Admin"),
