8c19e0eb8f124d3ea209a04c2b9a5b565ccce710
Arsian Kuo <arsiankuo86@gmail.com>
2025-05-14 17:46:19 +0800

    feature: 取得課程列表（所有人）

--- controllers/course.js
+++ controllers/course.js
@@ -3,7 +3,9 @@ const coachRepo = AppDataSource.getRepository("Coach");
 const courseRepo = AppDataSource.getRepository("Course");
 const courseChapterRepo = AppDataSource.getRepository("Course_Chapter");
 const { getAllCourseTypes } = require("../services/typeServices");
+const { filterByCategory } = require("../services/filterServices");
 const generateError = require("../utils/generateError");
+const paginate = require("../utils/paginate");
 const { isNotValidUUID } = require("../utils/validators"); // 引入驗證工具函數
 
 //取得課程類別（依照學生總人數排序）
@@ -37,6 +39,100 @@ async function getCoachType(req, res, next) {
         sort: "desc",
         sort_by: "popular",
         type: "coach", //幫助前端判斷顯示的是課程列表還是教練列表
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+//取得課程列表
+async function getCourses(req, res, next) {
+  try {
+    //禁止前端亂輸入參數，如banana=999
+    const validQuerys = ["page", "sortBy", "category", "skillId"];
+    const queryKeys = Object.keys(req.query);
+    const invalidQuerys = queryKeys.filter((key) => !validQuerys.includes(key));
+    if (invalidQuerys.length > 0) {
+      return next(generateError(400, `不允許的參數：${invalidQuerys.join(", ")}`));
+    }
+    //排序設定
+    const sort = "DESC"; //後端寫死
+    const sortBy = req.query.sortBy || "popular"; //預設按照熱門程度排序
+    const validSortBy = ["popular", "score"];
+    if (!validSortBy.includes(sortBy)) return next(generateError(400, "無此排序方式"));
+    //在撈資料前預先設定排序方式對應的參數
+    const validSortParams = {
+      popular: "c.student_amount",
+      score: "c.score",
+    };
+    //根據sortBy query取出參數
+    const sortParam = validSortParams[sortBy];
+    if (!sortParam) {
+      return next(generateError(400, "排序欄位參數不正確"));
+    }
+    //取得課程資料
+    const rawCourses = await AppDataSource.getRepository("Course")
+      .createQueryBuilder("c") //c=Course
+      .innerJoin("c.Skill", "s") //s=Skill
+      .innerJoin("c.Coach", "coach")
+      .select([
+        "c.id AS course_id", //課程id
+        "c.name AS course_name", //課程名稱
+        "c.description AS course_description", //課程介紹
+        "c.score AS course_score", //課程評分
+        "c.total_hours AS total_hours", //課程總時長
+        "c.image_url AS course_image_url", //課程封面
+        "s.id AS type_id", //課程類別id
+        "s.name AS course_type", //課程類別名稱
+        "c.student_amount AS student_amount", //課程學生人數
+        "coach.nickname AS coach_name", //教練名稱
+        "coach.job_title AS coach_title", //教練title
+      ])
+      .orderBy(sortParam, "DESC") //根據前端參數，載入排序設定
+      .getRawMany();
+
+    //分類設定
+    const category = req.query.category || "all"; //當前顯示類別，預設顯示所有類別
+    const validCategories = ["all", "skill"]; //所有類別、已收藏、特定類別（如：瑜伽）
+    if (!validCategories.includes(category)) return next(generateError(400, "無此類別"));
+    let filteredCourses;
+    if (category === "skill") {
+      const skillId = req.query.skillId; //若category="skill"，前端再回傳一個參數skillId
+      if (!skillId || isNotValidUUID(skillId))
+        return next(generateError(400, "類別為 skill 時必須提供合法的 skillId"));
+      //取得對應分類的資料
+      filteredCourses = await filterByCategory(rawCourses, category, skillId);
+    } else {
+      //取得對應分類的資料
+      filteredCourses = await filterByCategory(rawCourses, category);
+    }
+
+    //分頁設定
+    const page = parseInt(req.query.page) || 1; //當前頁數
+    const limit = 9; //每頁最多顯示9堂課程
+    if (isNaN(page) || page < 1 || !Number.isInteger(page)) {
+      return next(generateError(400, "分頁參數格式不正確，頁數需為正整數"));
+    }
+    //取得當前分頁資料，以及分頁資訊
+    const { paginatedData, pagination } = await paginate(filteredCourses, page, limit);
+    //若頁數超出範圍，回傳錯誤
+    const totalPages = pagination.total_pages;
+    if (page > totalPages && totalPages !== 0) {
+      return next(generateError(400, "頁數超出範圍"));
+    }
+
+    res.status(200).json({
+      status: true,
+      message: "成功取得資料",
+      data: paginatedData,
+      meta: {
+        filter: {
+          category, //篩選類別
+          sort, //排序（desc/asc）
+          sortBy, //排序方式
+        },
+        pagination,
       },
     });
   } catch (error) {
@@ -152,6 +248,7 @@ async function getCourseDetails(req, res, next) {
 module.exports = {
   getCourseType,
   getCoachType,
+  getCourses,
   getCoachDetails,
   getCourseDetails,
 };
--- routes/course.js
+++ routes/course.js
@@ -4,10 +4,17 @@ const courseController = require("../controllers/course");
 const ratingController = require("../controllers/rating");
 const auth = require("../middlewares/auth");
 
+//固定路由
+//固定路由順序要放在動態路由前，如:userId，否則會被/:userId的路由攔截
+
 //取得課程類別
 router.get("/course-type", courseController.getCourseType);
 //取得教練類別
 router.get("/coach-type", courseController.getCoachType);
+//取得課程列表
+router.get("/", courseController.getCourses);
+
+//動態路由
 //取得課程評價
 router.get("/:courseId/ratings", ratingController.getRatings);
 //取得教練詳細資訊
