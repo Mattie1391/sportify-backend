a83fc45d092f7851123636bc831eab2b1d71a67e
Job-Wilhelm <innocent87063@gmail.com>
2025-05-26 22:43:06 +0800

    建好路由

--- routes/coach.js
+++ routes/coach.js
@@ -8,6 +8,8 @@ const coachController = require("../controllers/coach");
 //固定路由
 //取得所有課程觀看資料，或依照輸入的課程id找對應課程
 router.get("/courses/views", auth, isCoach, coachController.getCoachViewStats);
+//教練後台取得自己課程列表
+router.get("/courses", auth, isCoach, coachController.getCoachCourses);
 
 //動態路由
 
