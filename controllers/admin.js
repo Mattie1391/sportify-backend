e18ceee8b3afce4f56e80751eb0320473b703405
Nikka <kyra11024@gmail.com>
2025-06-26 17:27:51 +0800

    fix: 加入管理員通知教練審核是否通過的信件通知

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,6 +13,9 @@ const { Mux } = require("@mux/mux-node");
 const mux = new Mux();
 const config = require("../config/index");
 const { muxSigningKeyForPublic, muxSigningKeySecretForPublic } = config.get("mux");
+const nodemailer = require("nodemailer");
+const gmailUserName = config.get("email.gmailUserName");
+const gmailAppPassword = config.get("email.gmailAppPassword");
 //services
 const { getAllCourseTypes } = require("../services/typeServices");
 const { courseFilter, coachFilter } = require("../services/filterServices");
@@ -29,6 +32,35 @@ const generateError = require("../utils/generateError");
 const paginate = require("../utils/paginate");
 const { parseYYYYMMDD } = require("../utils/formatDate");
 const { formatDate, addDays } = require("../utils/formatDate");
+
+
+function sendReviewEmail (email, subject, text, reviewComment) {
+  // 創建郵件傳輸器
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // 使用 Gmail 作為電子郵件服務
+    auth: {
+      user: gmailUserName, // 發送郵件的 Gmail 帳號
+      pass: gmailAppPassword, // 從環境變數讀取應用程式專用密碼
+    },
+  });
+
+  // 定義要發送的郵件內容
+  const mailOptions = {
+    from: "Sportify Plus <sportifyplus2025@gmail.com>", // 寄件者名稱和電子郵件
+    to: email, // 收件者的電子郵件地址
+    subject: subject, // 郵件主旨
+    text: text, // 純文字內容
+    html: `<a href="https://tteddhuang.github.io/sportify-plus/"><p>立即前往Sportify+查看</p></a><p>${reviewComment}`, // HTML 格式內容
+  };
+
+  // 發送郵件
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      // 如果發送失敗，回傳 HTTP 錯誤
+      return next(generateError(500, "無法發送電子郵件，請稍後再試"));
+    }
+  });  
+}
 
 //新增訂閱方案，目前沒有畫管理員相應UI的線稿
 async function postPlan(req, res, next) {
@@ -475,14 +507,29 @@ async function patchReviewCourse(req, res, next) {
     }
 
     await courseRepo.save(course);
+    
+    //取得課程對應的教練資料
+    const coach = await coachRepo.findOneBy({ id: course.coach_id });
 
     //回傳訊息
     if (status === "approved") {
+      sendReviewEmail(
+        coach.email,
+        "Sportify+ 課程審核通過通知",
+        `恭喜！您的課程 "${course.name}" 已通過審核`,
+        reviewComment
+      )
       return res.status(200).json({
         status: true,
         message: "課程審核成功，狀態已更新為 approved",
       });
     } else {
+      sendReviewEmail(
+        coach.email,
+        "Sportify+ 課程審核未通過通知",
+        `很抱歉！您的課程 "${course.name}" 未通過審核`,
+        reviewComment
+      )
       return res.status(200).json({
         status: true,
         message: "課程審核未通過，狀態已更新為 rejected",
@@ -521,11 +568,23 @@ async function patchReviewCoach(req, res, next) {
 
     // 回傳訊息
     if (status === "approved") {
+      sendReviewEmail(
+        coach.email,
+        "Sportify+ 教練資格審核通過通知",
+        `恭喜！您的教練資格已通過審核`,
+        reviewComment
+      )
       return res.status(200).json({
         status: true,
         message: "教練資格審核成功，狀態已更新為 approved",
       });
     } else {
+      sendReviewEmail(
+        coach.email,
+        "Sportify+ 教練資格審核未通過通知",
+        `很抱歉！您的教練資格未通過審核`,
+        reviewComment
+      )
       return res.status(200).json({
         status: true,
         message: "教練資格審核未通過，狀態已更新為 rejected",
